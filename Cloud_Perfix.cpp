fb628351aac71ef8a2f6c34a7a78bb75ced80eb1
SastaFlash <95137718+SwaritMisro12@users.noreply.github.com>
2023-09-01 08:36:24 -0700

    Add files via upload

---
+++ Cloud_Perfix.cpp
@@ -0,0 +1,88 @@
+#include <.h>
+#include <stdio.h>
+
+
+
+/*system is defined local users to allow to connect to specific ip
+NEED TO FIX THE PORT ISSUE */
+
+void static Cloud_Perfix()
+   if IP =  "192.168.1.1".orgs > std::typedef int OpenSSH;
+    allow_ssh = defined.local::std >+ main_function 
+     else	
+     system.point_Connectivity = ("You'r not able to connect to "(SYSTEM = ips_underlowered).acesspoint = defined_function)
+      return.point_Connectivity = "NULL".nd
+   return allow_ssh
+   
+
+void struct Cloud_Perfix
+{
+	class Point_Activity = point_Connectivity
+
+    while max = function("Make Sure To Allow".conosle_logs)
+	echo > conosle + main + defined + SSH + CLIENT + BServer
+
+	if IP = defined_Main[conosle.main_function = max.main_ZERO].args
+
+	 number_defined >> OpenSSH("DEFINED".function) 
+	 fix_cloud = std::map<10, 10> std::map;
+};
+
+
+
+int OpenSSH = Ports
+  struct = function.local::std(allow_users.ports).system_d 
+if ports = bool true ('ARGS_MAIN_FUNCTION_IPS_PORTSAGES'.run_console_logs)
+{
+    ports("22".default_IPS)
+    ips("***********".system_d)
+        printf("PORTS = BLOCKED",("UNLOCK BY THE PROVIDER USING FIREWALLS IN VPS/VDS").print_try_users_Ips);
+    allow_ssh = users.defined_BLOCKED::map PORTS 
+}
+
+
+
+if Ports_22 = define.system_d["22 IS BLOCKED TRY DIFFERENT".args] >> deined_Blockaged ++ PORTS 
+  int ports::std::map<10, args_main("Function system is runed").alerts_console_blockaged> map;	
+
+void static ports_22 
+   deined_Blockaged().ports 
+   {
+   	hasmap::users
+   	echo ==> sudo allow_ssh.sh
+   	List("22".num_defined =['22 to 30'])
+
+   }
+   return deined_Blockaged_ports = defined_alers::system_d("".conosle_logs)  
+
+
+
+
+
+ if static ips_underlowered("function".args)
+ 	  min.files_fixed('QUEUED'.Allow_System_D)
+ 	echo::system_d = hasmap > local_users
+    return main_function = defined ports('ARGS_MAIN_FUNCTION_IPS_PORTSAGES')
+       int ports = point_Connectivity.system_d("int_main =  fix_cloud")
+
+   fix_cloud = Rated.main_ZERO
+
+endpoint_details 'Array.point'
+
+
+
+void static arrays_pointed('ARGS_MAIN_FUNCTION_IPS_PORTSAGES') 
+     if arrays_pointed > pointed defined_BLOCKED::std('local_users'.args:std:std::map<10, 10> map;)
+    <=> if static pointed_occured =  bool:true ('Allow_heavy_points'.deined_Blockage
+       /* make the ssh allow the ports to accept or disallow the points of  ports to connect into  the specific SSL IPS*/
+       ? include allow_ssh ('input.168.192.1.1'.static_defined_pointed)  
+       systemctl.d occurs = input.main_ZERO - function("connect".allow_ssh) 
+
+
+  if systemctl_allow = local_users(input.has_id) 
+    allow_ssh('Specific-users')
+  main_ZERO + allow_ssh + acesspoint
+
+
+
+
